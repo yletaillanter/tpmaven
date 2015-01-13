d2ba824cb74eba1e2cbc1b3e489686b3a806aede
yletaillanter <y.letaillanter@gmail.com>
2015-01-13 14:49:07 +0100

    Initial Commit

---
+++ src/main/java/fr/istic/tpsir/tpmaven/App.java
@@ -0,0 +1,29 @@
+package fr.istic.tpsir.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    private String deadVariable;
+
+    public static void main( String[] args )
+    {
+    	
+    	/**
+    	 * bonbon!
+    	 *
+    	 */
+        System.out.println( "Hello World!" );
+    }
+
+    String test = "test";
+
+    public int jeDouble(int a){
+        return a*2;
+    }
+
+    public int jeDoubleAussi(int a){
+        return a*2;
+    }
+}
---
+++ src/test/java/fr/istic/tpsir/tpmaven/AppTest.java
@@ -0,0 +1,48 @@
+package fr.istic.tpsir.tpmaven;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        App app = new App();
+        assertTrue( true );
+        assertTrue(app.test.equals("test"));
+    }
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp2()
+    {
+        assertTrue( true );
+    }
+
+}
